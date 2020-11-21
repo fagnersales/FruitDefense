62924df7fcafafc679fa34a8c3cff407841324a9
Fagner Sales <fs.fagnersales@gmail.com>
2020-11-21 14:38:07 -0300

    update: State save way

--- State/index.js
+++ State/index.js
@@ -1,31 +1,31 @@
 const { readFileSync, writeFileSync } = require('fs')
 
 const read = _ => JSON.parse(readFileSync('./State/data.json', 'utf8'))
-const save = newFileData => writeFileSync('./State/data.json', JSON.stringify(newFileData, null, 2))
 
+const state = read()
+
+const saveState = () => {
+    writeFileSync('./State/data.json', JSON.stringify(state, null, 2))
+}
 
 const set = (key, value) => {
-    const data = read()
-    data[key] = value
-    save(data)
+    state[key] = value
+    saveState()
 }
 
 const remove = (key) => {
-    const data = read()
-    if (data[key]) delete data[key]
-    save(data)
+    if (state[key]) delete state[key]
+    saveState()
 }
 
 const get = key => {
-    const data = read()
-    return data[key]
+    return state[key]
 }
 
 const push = (key, values) => {
-    const data = read()
     if (!Array.isArray(values)) values = [values]
-    data[key] = data[key] ? [...data[key], ...values] : values
-    save(data)
+    state[key] = state[key] ? [...state[key], ...values] : values
+    saveState()
 }
 
 const reset = () => set({})
